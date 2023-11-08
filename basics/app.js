50c0f3aea0b9ca20a67b177c8c6b755bfe7e51ae
Qunnnn <TranVanQuan20211@gmail.com>
2023-11-08 18:34:55 +0700

    Routing request

--- basics/app.js
+++ basics/app.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     const url = req.url;
 
+    
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>');

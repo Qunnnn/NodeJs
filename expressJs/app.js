585a1acba60a9b5a505a642c44e9836b9b015390
Qunnn <TranVanQuan20211@gmail.com>
2023-11-23 23:46:30 +0700

    Working with middleware in Expressjs

--- expressJs/app.js
+++ expressJs/app.js
@@ -4,6 +4,17 @@ const express = require('express');
 
 const app = express();
 
+app.use((req,res,next) => {
+    console.log('In the middleware');
+    next();
+});
+
+
+app.use((req,res,next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from ExpressJs</h1>');  
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);

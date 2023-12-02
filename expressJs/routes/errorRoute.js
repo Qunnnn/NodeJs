77fbefebe1c905f29a24ebea34174f6d8bb65605
Qunnnn <TranVanQuan20211@gmail.com>
2023-12-02 21:06:12 +0700

    Added function helper

--- expressJs/routes/errorRoute.js
+++ expressJs/routes/errorRoute.js
@@ -1,11 +1,13 @@
 const path = require('path');
 
+const rootDir = require('../utils/root');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '../', 'views', 'error_page.html'));
+  res.status(404).sendFile(path.join(rootDir, 'views', 'error_page.html'));
 }
 )
 

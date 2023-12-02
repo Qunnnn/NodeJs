578304fdeac5a80c259bf946898755e5d7672186
Qunnnn <TranVanQuan20211@gmail.com>
2023-12-02 15:43:52 +0700

    Using ExpressJs Router

--- expressJs/app.js
+++ expressJs/app.js
@@ -3,23 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: true}),);
 
-app.use('/add-product', (req, res, next) => {
-    console.log(2),
-    res.send('<form action = "/product" method="POST"><input type = "text" name ="title"><button type="submit">Add product</button></form>');
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(4);
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log(3),
-        res.send('<h1>Hello from ExpressJs</h1>');
-});
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 app.listen(3000);
 

d57454ad6cf00045b81e50dc5aebc872bc714a1a
Qunnnn <TranVanQuan20211@gmail.com>
2023-12-02 22:39:13 +0700

    Serving files statically

--- expressJs/app.js
+++ expressJs/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -8,6 +9,7 @@ const shopRoutes = require('./routes/shop');
 const errorRoutes = require('./routes/errorRoute');
 
 app.use(bodyParser.urlencoded({extended: true}),);
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(shopRoutes);    
 app.use('/admin',adminRoutes);

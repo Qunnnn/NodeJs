bc74e9c232d8b1eb86b28644ffdf4996e90d17ba
Qunnnn <TranVanQuan20211@gmail.com>
2023-12-02 20:54:21 +0700

    Added error page

--- expressJs/routes/errorRoute.js
+++ expressJs/routes/errorRoute.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
-router.use((req,res,next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+router.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, '../', 'views', 'error_page.html'));
 }
 )
 

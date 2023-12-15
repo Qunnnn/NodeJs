091f497465d070fb6eb40ef42b0946629d8befc2
Qunnnn <TranVanQuan20211@gmail.com>
2023-12-15 11:10:43 +0700

    Working with ejs

--- expressJs/app.js
+++ expressJs/app.js
@@ -1,18 +1,24 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorRoutes = require('./routes/errorRoute');
 
-app.use(bodyParser.urlencoded({extended: true}),);
-app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(shopRoutes);    
-app.use('/admin',adminRoutes.routes);
-app.use(errorRoutes);
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3000);

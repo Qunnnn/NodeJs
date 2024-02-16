4427b206dd6ef14e6b44f742658e2316c0096aba
Qunnnn <TranVanQuan20211@gmail.com>
2024-02-16 21:44:31 +0700

    fetch single product

--- expressJs/controllers/shop.js
+++ expressJs/controllers/shop.js
@@ -13,17 +13,20 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+
+  Product.findById(prodId).then(([product]) => {
+    console.log([product]);
     res.render('shop/product-detail', {
-      product: product,
+      product: product[0],
       pageTitle: product.title,
       path: '/products'
-    });
-  });
+    })
+  }).catch(err => console.log(err));
+
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll().then(([rows, fieldData])=> {
+  Product.fetchAll().then(([rows, fieldData]) => {
     res.render('shop/index', {
       prods: rows,
       pageTitle: 'Shop',

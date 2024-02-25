db40121a9ba4dce08043a3ecb5781dbbe075cdab
Qunnnn <TranVanQuan20211@gmail.com>
2024-02-25 12:48:48 +0700

    Reseting the cart & fetching and outputting orders

--- expressJs/controllers/shop.js
+++ expressJs/controllers/shop.js
@@ -108,22 +108,27 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then(cart => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then(products => {
       return req.user.createOrder()
         .then(order => {
           return order.addProducts(products.map(product => {
-            product.orderItem = { quantity: product.cartItem.quantity};
+            product.orderItem = { quantity: product.cartItem.quantity };
             return product;
           }))
         })
         .catch(err => console.log(err));
     })
     .then(() => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(()=> {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
@@ -131,15 +136,14 @@ exports.postOrder = (req, res, next) => {
 
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
-};
+  req.user
+    .getOrders({ include: ['products'] })
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders,
+      });
+    })
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
 };

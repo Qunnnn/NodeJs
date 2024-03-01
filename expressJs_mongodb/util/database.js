7691a894583cef0a09dfbcc3d01a0b58f1e4d559
Qunnnn <TranVanQuan20211@gmail.com>
2024-03-01 23:00:47 +0700

    Installing the mongoDB

--- expressJs_mongodb/util/database.js
+++ expressJs_mongodb/util/database.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '20092001', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://qun:123123qw@cluster0.x7tx4zt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(client => {
+    console.log('Connected!');
+    callback(client);
+})
+.catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;

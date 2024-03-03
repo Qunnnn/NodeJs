a76c5f824fbcd5243bd9f69540ffb9342d0781b2
Qunnnn <TranVanQuan20211@gmail.com>
2024-03-03 21:24:32 +0700

    Finish connect to Database

--- expressJs_mongodb/util/database.js
+++ expressJs_mongodb/util/database.js
@@ -1,13 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://qun:123123qw@cluster0.x7tx4zt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-.then(client => {
-    console.log('Connected!');
-    callback(client);
-})
-.catch(err => console.log(err));
+let _db;
+
+const mongoConnect = callback => {
+    MongoClient.connect(
+        'mongodb+srv://qun:123123qw@cluster0.x7tx4zt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+      )
+        .then(client => {
+          console.log('Connected!');
+          _db = client.db();
+          callback();
+        })
+        .catch(err => {
+          console.log(err);
+          throw err;
+        });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+        throw 'No database found!'; 
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;

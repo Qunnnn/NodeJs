47e2a40e2ba25d8e49edf368ca140784c4f9ccc4
Qunnnn <TranVanQuan20211@gmail.com>
2024-03-06 21:53:47 +0700

    Fetching all products

--- expressJs_mongodb/models/product.js
+++ expressJs_mongodb/models/product.js
@@ -11,13 +11,27 @@ class Product {
   save() {
     const db = getDb();
     return db.collection('products').insertOne(this)
-    .then(result => {
-      console.log(result);
-    })
-    .catch(err => {
-       console.log(err);
-      }); 
-   }
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+
+    return db.collection('products').
+    find()
+    .toArray()
+      .then(products => {
+        return products;
+       })
+      .catch(err => {
+        console.log(err);
+      })
+  }
 }
 
 module.exports = Product;

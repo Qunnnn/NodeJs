aecc7664b5c887f8f43534b1373265b5cb9ff74c
Qunnnn <TranVanQuan20211@gmail.com>
2024-03-18 23:33:27 +0700

    Updated the way to pass _id in products model

--- expressJs_mongodb/controllers/admin.js
+++ expressJs_mongodb/controllers/admin.js
@@ -1,7 +1,4 @@
 const Product = require('../models/product');
-const mongodb = require('mongodb');
-
-const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -54,7 +51,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-  const product = new Product(updatedTitle, updatedPrice, updatedImageUrl, updatedDesc, new ObjectId(prodId));
+  const product = new Product(updatedTitle, updatedPrice, updatedImageUrl, updatedDesc, prodId);
 
   product.save().then(() => {
     console.log('Updated product successfully!');

42aa9b4bdd7397c815e7ef01b347b2fd172692dc
Qunnnn <TranVanQuan20211@gmail.com>
2024-03-20 22:17:36 +0700

    Adding product to cart with addToCart method in User model

--- expressJs_mongodb/app.js
+++ expressJs_mongodb/app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('65faf15bf42e2d0377fe4030')
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name , user.email , user.cart , user._id);
             next();
         })
         .catch(err => {

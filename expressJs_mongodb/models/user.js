f85f731fb86a8ad427907a6d96857ba481ac1313
Qunnnn <TranVanQuan20211@gmail.com>
2024-03-21 20:16:23 +0700

    Storing multiples products in the Cart

--- expressJs_mongodb/models/user.js
+++ expressJs_mongodb/models/user.js
@@ -1,10 +1,10 @@
 const mongodb = require('mongodb');
-const getDb = require ('../util/database').getDb;
+const getDb = require('../util/database').getDb;
 
 const ObjectId = mongodb.ObjectId;
 
 class User {
-    constructor(username, email , cart , id) {
+    constructor(username, email, cart, id) {
         this.username = username;
         this.email = email;
         this.cart = cart; // {items: []}
@@ -17,25 +17,37 @@ class User {
     }
 
     addToCart(product) {
-        // const cartProduct = this.cart.items.findIndex(cp => {
-        //     return cp._id === product._id;
-        // });
+        const cartProductIndex = this.cart.items.findIndex(cp => {
+            //Compare the product id in the cart with the product id in the product collection with String type it will not work in ObjectId type
+            return cp.productId.toString() === product._id.toString();
+        });
 
-      
-        const updatedCart = { items: [{ productId: product._id , quantity: 1 }] };
-        
+        let newQuantity = 1;
+        const updatedCartItems = [...this.cart.items];
+
+        if (cartProductIndex >= 0) {
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity;
+        } else {
+            updatedCartItems.push({ productId: new ObjectId(product._id), quantity: newQuantity });
+        }
+
+        const updatedCart = {
+            items: updatedCartItems
+        };
+
         const db = getDb();
         return db
-        .collection('users')
-        .updateOne(
-            { _id: new ObjectId(this._id) },
-            { $set: { cart: updatedCart } }
-        );
+            .collection('users')
+            .updateOne(
+                { _id: new ObjectId(this._id) },
+                { $set: { cart: updatedCart } }
+            );
     }
 
-   static findById(userId) {
-         const db = getDb();
-         return db.collection('users').findOne({ _id: new ObjectId(userId)});
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users').findOne({ _id: new ObjectId(userId) });
     }
 
 }

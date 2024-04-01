566d68fa4de0557aa2822664f438e2a2c1268cd9
Qunnnn <TranVanQuan20211@gmail.com>
2024-04-01 23:24:55 +0700

    Add user model

--- expressJs_mongodb/app.js
+++ expressJs_mongodb/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -16,16 +16,16 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, _res, next) => {
-//     User.findById('65faf15bf42e2d0377fe4030')
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         })
-//         .catch(err => {
-//             console.log(err);
-//         });
-// });
+app.use((req, _res, next) => {
+    User.findById('660adf9c6b6cf46628562875')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,8 +34,19 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://qun:123123qw@cluster0.x7tx4zt.mongodb.net/shop').then(
     result => {
-        app.listen(3000);
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Qun',
+                    email: 'qun@gmail.com',
+                    cart: {
+                        items: []
+                    },
+                });
+                user.save();
+                app.listen(3000);
+            }
+        });
     }).catch(err => {
         console.log(err);
-    }
-    )
+    });
